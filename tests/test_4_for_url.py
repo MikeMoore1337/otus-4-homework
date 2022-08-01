359185e47f90aec1b10a3d162db9d530a7af16ff
MikeMoore1337 <42142321+MikeMoore1337@users.noreply.github.com>
2022-08-01 20:17:43 +0200

    Update test_4_for_url.py

--- tests/test_4_for_url.py
+++ tests/test_4_for_url.py
@@ -3,7 +3,7 @@ import requests
 
 
 def test_url_and_status_code(url_param, status_code):
-    """Тест для проверки status_code"""
+    """Checking status_code"""
     response = requests.get(url_param)
     status_code = response.status_code
     assert status_code == 200
